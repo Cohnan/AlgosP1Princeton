bba9788aad012ae12ece719ae4b363e1e002e88f
Cohnan <sc.puerto10@uniandes.edu.co>
2019-01-26 13:45:11 -0500

    Minor implementations in Percolation

--- EclipseWorkspaceAlgs4/PercolationAlgs4/src/Percolation.java
+++ EclipseWorkspaceAlgs4/PercolationAlgs4/src/Percolation.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * A class whose objects represent the material, it knows nothing about the probability of openness
  */
 
 /**
@@ -8,21 +8,26 @@
  */
 public class Percolation {
 	// create n-by-n grid, with all sites blocked
+	private boolean[][] siteMatrix;
+	private int p;
+	
+	
 	public Percolation(int n) {
-		
+		//Java, by default, initializes arrays with null or 0 like values.
+		siteMatrix = new boolean[n][n]; 
 	}
 	
 	// open site (row, col) if it is not open already
 	public    void open(int row, int col) { 
-		
+		siteMatrix[row - 1][col - 1] = true;
 	}
 	
 	// is site (row, col) open?
 	public boolean isOpen(int row, int col) {
-		
+		return siteMatrix[row - 1][col - 1];
 	}
 	
-	// is site (row, col) full?
+	// is site (row, col) full? (connected to row 1 via some path)
 	public boolean isFull(int row, int col) {
 		
 	}
